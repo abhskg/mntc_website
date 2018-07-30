3079ea5a9b559baef96ec79ef527c10f7ecbdad6
tantrojan <tg.99ghosh@gmail.com>
2018-07-30 19:46:49 +0530

    Project Initiated

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Carousel from './components/carousel';
+
+class App extends Component {
+  render() {
+    return (
+      <Carousel />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/carousel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './carousel.css';
+
+class Carousel extends Component {
+  render() {
+    return (
+    	<div className='image_carousel'>
+      		<h1> Inside Carousel </h1>
+      	</div>
+    )
+  }
+}
+
+export default Carousel;
